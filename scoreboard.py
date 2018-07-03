78bb74d283ba4c218cf670d40398d338e4f0d0a9
Jennifer <jennifer.du_@hotmail.com>
2018-07-03 23:16:28 +1000

    Add comment to end certificate

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,6 @@
 from pygame import font
 import pygameMenu
+from scipy.interpolate import interp1d
 
 from settings import Settings
 
@@ -41,6 +42,7 @@ class Scoreboard:
 	def show_end_state(self):
 
 		myfont = font.Font(pygameMenu.fonts.FONT_MUNRO, 60)
+		otherfont = font.Font(pygameMenu.fonts.FONT_MUNRO, 25)
 		win_state = myfont.render(
 			'WIN' if self.right_score > 10 else 'LOSE',
 			True,
@@ -52,11 +54,34 @@ class Scoreboard:
 			Settings.text_colour, 	#colour
 		)
 
+		comments = [
+			# comments for lose
+			'Oh no! Your plonking skills are getting a bit rusty!',
+			'Nice try. Keep at it!',
+			'So close. Better luck next time!',		
+			# comments for win
+			'That was a close one!',
+			'Woohoo! Good job!',
+			'You\'re killing it!',
+		]
+
+		score_difference = self.right_score - self.left_score 
+		# choose comment from list of commnents based on score difference, map score difference to comments
+		choose_comment = interp1d([-11, 11], [0,5.99])
+		comment_index = int(choose_comment(score_difference))
+
+		comment = otherfont.render(
+			comments[comment_index],
+			True,
+			Settings.text_colour,
+		)
+
 		win_state_width = win_state.get_rect().width
 		win_state_height = win_state.get_rect().height
 		scoreboard_width = scoreboard.get_rect().width
 		scoreboard_height = scoreboard.get_rect().height
+		comment_width = comment.get_rect().width
+		comment_height = comment.get_rect().height
 		self.screen.blit(win_state, (self.screen_width/2 - win_state_width/2, self.screen_height/2 - scoreboard_height*2))
 		self.screen.blit(scoreboard, (self.screen_width/2 - scoreboard_width/2, self.screen_height/2 - scoreboard_height/2))
-
-
+		self.screen.blit(comment, (self.screen_width/2 - comment_width/2, self.screen_height/2 + comment_height*2))
